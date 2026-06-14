f62e954627059c895109c8b3a584b2a09a7e9629
OutramM <outrammax@gmail.com>
2026-06-14 13:02:00 +0100

    Create team.js

---
+++ team.js
@@ -0,0 +1,39 @@
+const teams = [
+
+{name:"Argentina",rating:94,pot:1},
+{name:"France",rating:93,pot:1},
+{name:"Spain",rating:92,pot:1},
+{name:"England",rating:91,pot:1},
+{name:"Brazil",rating:90,pot:1},
+{name:"Portugal",rating:89,pot:1},
+{name:"Germany",rating:88,pot:1},
+{name:"Netherlands",rating:87,pot:1},
+
+{name:"Croatia",rating:84,pot:2},
+{name:"Morocco",rating:84,pot:2},
+{name:"Japan",rating:83,pot:2},
+{name:"Senegal",rating:82,pot:2},
+{name:"Mexico",rating:81,pot:2},
+{name:"USA",rating:81,pot:2},
+{name:"Switzerland",rating:81,pot:2},
+{name:"Denmark",rating:80,pot:2},
+
+{name:"Serbia",rating:78,pot:3},
+{name:"Poland",rating:78,pot:3},
+{name:"Turkey",rating:77,pot:3},
+{name:"South Korea",rating:77,pot:3},
+{name:"Canada",rating:76,pot:3},
+{name:"Nigeria",rating:76,pot:3},
+{name:"Australia",rating:75,pot:3},
+{name:"Egypt",rating:75,pot:3},
+
+{name:"Chile",rating:72,pot:4},
+{name:"Ireland",rating:72,pot:4},
+{name:"Norway",rating:72,pot:4},
+{name:"Iraq",rating:71,pot:4},
+{name:"Costa Rica",rating:70,pot:4},
+{name:"New Zealand",rating:69,pot:4},
+{name:"China",rating:68,pot:4},
+{name:"South Africa",rating:68,pot:4}
+
+];
